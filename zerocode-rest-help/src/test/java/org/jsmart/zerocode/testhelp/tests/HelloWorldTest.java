c25f6e4ae2cbeaf973b59bbd795cc4a4c733484f
Author jSmart <author.japps@gmail.com>
2017-12-21 07:55:42 +0700

    DOC # How to run HelloWorld, bit of doc

--- zerocode-rest-help/src/test/java/org/jsmart/zerocode/testhelp/tests/HelloWorldTest.java
+++ zerocode-rest-help/src/test/java/org/jsmart/zerocode/testhelp/tests/HelloWorldTest.java
@@ -10,14 +10,48 @@ import org.junit.runner.RunWith;
 @RunWith(ZeroCodeUnitRunner.class)
 public class HelloWorldTest {
 
-
     @Test
     @JsonTestCase("hello_world/hello_world_get.json")
     public void testHelloWorld_onlineRestApi() throws Exception {
 
     }
 
-
 }
 
+/*
 
+-----------------------------------------------------------------
+- "src/test/resources/hello_world/hello_world_get.json" contains:
+-----------------------------------------------------------------
+{
+    "scenarioName": "scenario_hello_world_get",
+    "steps": [
+        {
+            "name": "get_user_details",
+            "url": "/posts/1",
+            "operation": "GET",
+            "request": {
+            },
+            "assertions": {
+                "status": 200,
+                "body": {
+                    "id": 1,
+                    "userId": 1,
+                    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+                }
+            }
+        }
+    ]
+}
+
+------------------------------------------------------------
+-  "rc/test/resources/hello_world_host.properties" contains:
+------------------------------------------------------------
+# Web Server host and port
+restful.application.endpoint.host=https://jsonplaceholder.typicode.com
+restful.application.endpoint.port=443
+
+# Web Service context; Leave it blank in case you do not have a common context
+restful.application.endpoint.context=
+
+*/
