6879b92cb16cafc24ad09b45661fed5c350ca215
Author jSmart <author.japps@gmail.com>
2018-02-17 15:41:30 +0000

    ISSUE-28 # SOAP 1.1 and SOAP 1.2 end points

--- zerocode-rest-help/src/test/java/org/jsmart/zerocode/testhelp/tests/soap/SoapEndPointTest.java
+++ zerocode-rest-help/src/test/java/org/jsmart/zerocode/testhelp/tests/soap/SoapEndPointTest.java
@@ -5,7 +5,6 @@ import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.domain.UseHttpClient;
 import org.jsmart.zerocode.core.httpclient.ssl.SslTrustHttpClient;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,9 +13,34 @@ import org.junit.runner.RunWith;
 @RunWith(ZeroCodeUnitRunner.class)
 public class SoapEndPointTest {
 
+    /**
+     * Visit HelloWorld:
+     * http://secure.smartbearsoftware.com/samples/testcomplete12/webservices/Service.asmx?op=HelloWorld
+     *
+     * All samples:
+     * http://secure.smartbearsoftware.com/samples/testcomplete12/webservices/Service.asmx#_ga=2.74525649.2109787819.1518852706-497778140.1518852706
+     */
+    @Test
+    @JsonTestCase("soap/soap_endpoint_hello_world_post_200.json")
+    public void testSoapHelloWorldWith_SOAPAction() throws Exception {
+
+    }
+
     @Test
     @JsonTestCase("soap/soap_endpoint_soap_action_post_200.json")
     public void testSoapEndPointWith_SOAPAction() throws Exception {
+
+    }
+
+    @Test
+    @JsonTestCase("soap/soap_endpoint_xml_node_type_response_200.json")
+    public void testSoapXmlResponse_SOAPAction() throws Exception {
+
+    }
+
+    @Test
+    @JsonTestCase("soap/soap1.2_endpoint_xml_type_response_200.json")
+    public void testSoap12With_contentTypeOnly() throws Exception {
 
     }
 }
